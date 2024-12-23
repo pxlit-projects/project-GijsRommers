2deb4f101d3be3c9d8c47c572f1b68cbe926a005
Gijs Rommers <contactnanogijs@gmail.com>
2024-12-23 16:56:14 +0100

    logback added, up to rabbitMQ (Why not kafta??)

--- backend-java/ExamenProject/gateway-service/src/main/java/be/pxl/services/GatewayServiceApplication.java
+++ backend-java/ExamenProject/gateway-service/src/main/java/be/pxl/services/GatewayServiceApplication.java
@@ -3,16 +3,13 @@ package be.pxl.services;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
-public class PostServiceApplication
+public class GatewayServiceApplication
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(PostServiceApplication.class, args);
+        SpringApplication.run(GatewayServiceApplication.class, args);
     }
 }
