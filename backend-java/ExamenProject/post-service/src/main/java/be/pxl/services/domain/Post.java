485eee860051d9fd27c6635e98b5dd5bb37d7994
Gijs Rommers <contactnanogijs@gmail.com>
2024-12-23 18:52:51 +0100

    adding posts (draft/review)

--- backend-java/ExamenProject/post-service/src/main/java/be/pxl/services/domain/Post.java
+++ backend-java/ExamenProject/post-service/src/main/java/be/pxl/services/domain/Post.java
@@ -1,8 +1,7 @@
 package be.pxl.services.domain;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
--- backend-java/ExamenProject/post-service/src/main/java/be/pxl/services/services/PostService.java
+++ backend-java/ExamenProject/post-service/src/main/java/be/pxl/services/services/PostService.java
@@ -1,9 +1,13 @@
 package be.pxl.services.services;
 
 import be.pxl.services.api.dto.request.PostRequest;
+import be.pxl.services.domain.Post;
+import be.pxl.services.domain.PostStatus;
 import be.pxl.services.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -12,7 +16,13 @@ public class PostService implements IPostService {
     private final PostRepository postRepository;
 
     @Override
-    public void createPost(PostRequest post) {
-        // Implementation here
+    public void createPost(PostRequest postRequest) {
+        Post post = new Post();
+        post.setTitle(postRequest.title());
+        post.setContent(postRequest.content());
+        post.setAuthor(postRequest.author());
+        post.setCreatedAt(LocalDateTime.now());
+        post.setStatus(postRequest.isDraft() ? PostStatus.DRAFT : PostStatus.PENDING_APPROVAL );
+        postRepository.save(post);
     }
 }
