b08ffb091090d8dac2e83804b0f4868584836890
Gijs Rommers <contactnanogijs@gmail.com>
2024-12-23 15:26:56 +0100

    Service Discovery

---
+++ backend-java/ExamenProject/src/main/java/be/pxl/services/examenproject/ExamenProjectApplication.java
@@ -0,0 +1,13 @@
+package be.pxl.services.examenproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenProjectApplication.class, args);
+    }
+
+}
---
+++ backend-java/ExamenProject/src/test/java/be/pxl/services/examenproject/ExamenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package be.pxl.services.examenproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
