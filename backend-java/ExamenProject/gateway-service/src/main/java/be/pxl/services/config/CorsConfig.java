265bff834e3ab6b6031f5d2d5e883272b5f7ed22
Gijs Rommers <contactnanogijs@gmail.com>
2024-12-30 22:21:48 +0100

    fixed cors

---
+++ backend-java/ExamenProject/gateway-service/src/main/java/be/pxl/services/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package be.pxl.services.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.server.WebFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("http://localhost:4200");
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true);
+        return new CorsWebFilter(source -> corsConfig);
+    }
+}
--- frontend-web/java-frontend/src/app/components/home/home.component.ts
+++ frontend-web/java-frontend/src/app/components/home/home.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../../service/post/post.service';
 import { Post } from '../../models/post.model';
-import {DatePipe} from '@angular/common';
+import { DatePipe } from '@angular/common';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardSubtitle,
+  MatCardTitle
+} from '@angular/material/card';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   standalone: true,
   imports: [
-    DatePipe
+    DatePipe,
+    MatCard,
+    MatCardTitle,
+    MatCardContent,
+    MatCardActions,
+    MatCardSubtitle,
+    MatCardHeader,
+    MatButton
   ],
   styleUrls: ['./home.component.css']
 })
@@ -21,5 +37,9 @@ export class HomeComponent implements OnInit {
     this.postService.getPosts().subscribe((data: Post[]) => {
       this.posts = data;
     });
+  }
+
+  trackByPost(index: number, post: Post): number {
+    return post.id;
   }
 }
